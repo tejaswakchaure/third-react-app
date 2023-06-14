ff087f9afa3bceee5785faf1fd05adad291398d5
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-14 11:01:25 +0530

    button click

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import AppHeader from "./pages/AppHeader";
-import AppBody from "./pages/AppBody";
-import AppFooter from "./pages/AppFooter";
+
 
 //user define tag 
 
 function App() {
   return (
     <>
-      {/* //calling multiple function in app function */}
-
-      <AppHeader />
-      <AppBody />
-      <AppFooter />
+    <h1>Counter App</h1>
+    <CounterApp/>
     </>
   );
 
 }
+
+function CounterApp(){
+  // member function 
+  let likeMeAction = () => {
+    console.log("I m button click");
+  }
+  return(
+    <>
+    <input type="button" value="Like me" onClick={likeMeAction}/>
+    </>
+  );
+}
 export default App;
 
-//complier convert into es file ecma script 
-//build folder diploy at time of production
