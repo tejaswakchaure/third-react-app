ef7ce522b8a4827ce591be841a6a36eed28c9314
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-14 09:09:24 +0530

    multiple function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+//user define tag 
+
 function App() {
-  return
-  (
+  return (
     <>
-      <h1>Hello World</h1>
+    {/* //calling multiple function in app function */}
+    
+      <AppHeader/>
+      <AppBody/>
+      <AppFooter/>
     </>
   );
 
+}
+
+
+// it also user define tag <AppHeader/>
+function AppHeader(){
+  return(
+    <>
+    <h1>Header</h1>
+  </>
+  );
+}
+
+
+// it also user define tag <AppHeader/>
+function AppBody(){
+  return(
+    <>
+    <h1>AppBody</h1>
+  </>
+  );
+}
+
+// it also user define tag <AppFooter/>
+function AppFooter(){
+  return(
+    <>
+    <h1>AppFooter</h1>
+  </>
+  );
 }
 
 export default App;
