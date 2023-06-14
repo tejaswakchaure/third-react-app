a1692441d107bee48cacf926210e62ae0ef1bdcc
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-14 10:36:43 +0530

    body-update

--- src/App.js
+++ src/App.js
@@ -38,7 +38,21 @@ function AppBody(){
   return(
     <>
     <div className="alert alert-success fs-3">
-      lorem
+      <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
+        blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
+        quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
+        at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
+        aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
+        tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
+        ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
+        Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
+        ratione adipisci deleniti alias aspernatur voluptatem!
+      </div>
     </div>
   </>
   );
