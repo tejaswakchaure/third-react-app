f0be6e8386a9a5f9cc9b983beec365843d73db3a
Tejas Wakchaure <wakchauretejas2@gmail.com>
2023-06-14 10:44:25 +0530

    footer-update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@
 function App() {
   return (
     <>
-    {/* //calling multiple function in app function */}
-    
-      <AppHeader/>
-      <AppBody/>
-      <AppFooter/>
+      {/* //calling multiple function in app function */}
+
+      <AppHeader />
+      <AppBody />
+      <AppFooter />
     </>
   );
 
@@ -15,55 +15,59 @@ function App() {
 
 
 // it also user define tag <AppHeader/>
-function AppHeader(){
-  return(
+function AppHeader() {
+  return (
     <>
-    {/* <h1 className="bg-dark text-align-center">Header</h1> */}
+      {/* <h1 className="bg-dark text-align-center">Header</h1> */}
 
-    <div className="row  bg-success text-light p-3 fs-3">
-      <div className="col d-flex justify-content-center">
-        <div className="col">Home</div>
-        <div className="col">Project</div>
-        <div className="col">AboutUs</div>
+      <div className="row  bg-success text-light p-3 fs-3">
+        <div className="col d-flex justify-content-center">
+          <div className="col">Home</div>
+          <div className="col">Project</div>
+          <div className="col">AboutUs</div>
 
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
 
 
 // it also user define tag <AppHeader/>
-function AppBody(){
-  return(
+function AppBody() {
+  return (
     <>
-    <div className="alert alert-success fs-3">
-      <div>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
-        rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
-        blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
-        quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
-        at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
-        aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
-        tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
-        ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
-        amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
-        Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
-        ratione adipisci deleniti alias aspernatur voluptatem!
+      <div className="alert alert-success fs-3 ">
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+          rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
+          blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
+          quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
+          at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
+          aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
+          tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
+          ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
+          Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
+          ratione adipisci deleniti alias aspernatur voluptatem!
+        </div>
       </div>
-    </div>
-  </>
+    </>
   );
 }
 
 // it also user define tag <AppFooter/>
-function AppFooter(){
-  return(
+function AppFooter() {
+  return (
     <>
-    <h1>AppFooter</h1>
-  </>
+      <div className="bg-dark text-light fs-1 d-flex justify-content-center aligb-item-center mt-0"
+        style={{ height: "200px" }}>
+        Lorem ipsum dolor sit amet.
+      </div>
+    </>
+
   );
 }
 
